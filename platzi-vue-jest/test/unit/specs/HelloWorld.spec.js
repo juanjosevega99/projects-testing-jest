ce54b0c08dd367af1f9f5ea79e22e0aa6aa5f1e5
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-10-24 10:15:53 -0500

    [TEST] vue render data and snapshot

---
+++ platzi-vue-jest/test/unit/specs/HelloWorld.spec.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld";
+
+describe("HelloWorld.vue", () => {
+  it("should render correct contents", () => {
+    const Constructor = Vue.extend(HelloWorld);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector(".hello h1").textContent).toEqual("Hello Vue");
+  });
+
+  test("Create snapshot with vue", () => {
+    const Constructor = Vue.extend(HelloWorld);
+    const vm = new Constructor().$mount();
+    expect(vm.$el).toMatchSnapshot;
+  });
+});
