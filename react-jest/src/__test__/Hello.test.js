6f5277531469b587f40ad49af377ff9179e65686
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-10-24 10:03:21 -0500

    [TEST] received props

---
+++ react-jest/src/__test__/Hello.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hello from "../components/Hello";
+import Enzyme, { mount, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Add test to React", () => {
+  test("test the text received for prop", () => {
+    const data = { title: "Hi Jest" };
+    const wrapper = mount(<Hello title={data.title} />);
+    const h1 = wrapper.find("h1");
+    expect(h1.text()).toBe("Hi Jest");
+  });
+});
